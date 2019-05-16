1b5946a94fffd1c5d43a20e5660b87169b6052c6
Phong Truong <phongoli0@gmail.com>
2019-05-16 16:41:58 -0700

    completed feature set 1

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import Header from './header';
+import GradeTable from './grade-table';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+    this.getAllGrades = this.getAllGrades.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllGrades();
+  }
+
+  getAllGrades() {
+    fetch('/api/grades')
+      .then(grades => grades.json())
+      .then(grades => {
+        this.setState({ grades: grades });
+      });
+  }
+
   render() {
-    return null;
+    return (
+      <div>
+        <Header />
+        <GradeTable grades={this.state.grades}/>
+      </div>
+    );
   }
 }
 
---
+++ client/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <div>
+      <h1 className={'col-xs-12'}>Student Grade Table</h1>
+    </div>
+  );
+}
+
+export default Header;
