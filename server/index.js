51b38aa2190d66a54fd2e4ba61df4b852296da7b
Phong Truong <phongoli0@gmail.com>
2019-08-22 13:25:51 -0700

    build

--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
+// const path = require('path');
+// const express = require('express');
+// const jsonServer = require('json-server');
+
+// const dbPath = path.resolve(__dirname, '../database/db.json');
+// const server = jsonServer.create();
+// const endpoints = jsonServer.router(dbPath);
+// const PORT = process.env.PORT;
+
+// server.use(express.json());
+// server.use(express.static(path.join(__dirname, 'public')));
+// server.use('/api', endpoints);
+// server.listen(PORT, () => {
+//   // eslint-disable-next-line no-console
+//   console.log('JSON Server listening on port', PORT);
+// });
 const path = require('path');
-const express = require('express');
 const jsonServer = require('json-server');
 
 const dbPath = path.resolve(__dirname, '../database/db.json');
 const server = jsonServer.create();
+const middleware = jsonServer.defaults();
 const endpoints = jsonServer.router(dbPath);
-const PORT = process.env.PORT;
 
-server.use(express.json());
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(middleware);
 server.use('/api', endpoints);
-server.listen(PORT, () => {
+server.listen(3001, () => {
   // eslint-disable-next-line no-console
-  console.log('JSON Server listening on port', PORT);
+  console.log('JSON Server listening on port 3001\n');
 });
